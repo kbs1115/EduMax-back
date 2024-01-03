96c7e0727f5c5a6eb75eecf321e0f5f153bcb9e0
윤규민 <dbsrbals26@gmail.com>
2024-01-04 00:26:28 +0900

    add sample communiry model test_code

--- tests/community/model_test.py
+++ tests/community/model_test.py
@@ -1,7 +1,29 @@
 import pytest
+
+from account.models import User
 from community.models import Post
 
 
-@pytest.fixture(scope='module')
-def mocked_post_instance(mocker):
+@pytest.fixture(scope='function')
+def mocked_post_instance(mocker, monkeypatch):
+    pass
+
+
+class TestPostModel:
+    pass
+
+
+class TestCommentModel:
+    pass
+
+
+class TestAlarmModel:
+    pass
+
+
+class TestLikeModel:
+    pass
+
+
+class TestLectureModel:
     pass
