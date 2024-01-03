8ff58ea14b308f997127c8bf808e321390564407
윤규민 <dbsrbals26@gmail.com>
2024-01-04 00:25:39 +0900

    make sample api view code

--- community/views.py
+++ community/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from service.post_service import PostService
 
-# Create your views here.
+
+class PostView(APIView):
+    def __init__(self):
+        self.post_service = PostService()
+
+    # 관심사 분리
+    def get(self, request):
+        query_params = request.GET
+        title = query_params.get("title")
+        posts = self.post_service.get_posts(title)  # json 형식으로 return
+        return JsonResponse(data=posts)
