22a1df868ce082edaf08b5fb13a1ac654d3ebcd1
kbs1115 <bruce11158@gmail.com>
2024-01-04 14:37:52 +0900

    postman test용 viewset 완성 + 작동 확인

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
-from .views import AuthAPIView, SignUpAPIView
+from django.urls import path, include
+from .views import AuthAPIView, SignUpAPIView, TestViewSet
 from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register("test", TestViewSet)
 
 urlpatterns = [
     path("signup/", SignUpAPIView.as_view()),
     path("token/", AuthAPIView.as_view()),
     path("token/refresh/", TokenRefreshView.as_view()),
+    path("", include(router.urls)),
 ]
