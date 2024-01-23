6e1b4d75b8b04098c1090abc837ab4974c227a9d
kbs1115 <bruce11158@gmail.com>
2024-01-24 07:25:28 +0900

    django URL 404 에러 json response로 처리 (DEBUG =False에서 작동)

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("account.urls")),
 ]
+
+handler404 = "config.exceptions.custom_exception_views.url_not_found"
