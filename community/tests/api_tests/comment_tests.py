017cade3961371ce30d1c3833617c29ac236edb9
kbs1115 <bruce11158@gmail.com>
2024-02-06 11:35:52 +0900

    Comment API Test File 추가

--- community/tests/api_tests/comment_tests.py
+++ community/tests/api_tests/comment_tests.py
@@ -2,24 +2,35 @@ import pytest
 from django.urls import reverse
 
 from community.tests.api_tests.conftests import *
+from community.model.models import File, Comment
 
 
 class TestMakeCommentToPostAPI:
     @pytest.mark.django_db
-    def test_create_comment(self, client, logined_client, setup_data):
+    def test_create_comment(
+        self, client, logined_client, setup_data, setup_files, mocked_create_files
+    ):
         endpoint = reverse("community:post_comment", kwargs={"post_id": 2})
 
         # 로그인된 유저 테스트
-        response = logined_client[0].post(
-            endpoint,
-            {"content": "testcontent3", "html_content": "htmltestcontent3"},
-            format="json",
-        )
+        with open(setup_files[0].name, "rb") as image1, open(
+            setup_files[1].name, "rb"
+        ) as image2:
+            response = logined_client[0].post(
+                endpoint,
+                {
+                    "content": "testcontent3",
+                    "html_content": "htmltestcontent3",
+                    "files": [image1, image2],
+                },
+                format="multipart",
+            )
 
         assert response.status_code == 201
         assert response.data["data"]["post_id"] == 2
         assert response.data["data"]["author"] == setup_data[4].login_id
         assert Comment.objects.get(post_id=2)
+        mocked_create_files.assert_called_once()
 
         # 로그인 안 된 유저 테스트
         response = client.post(
@@ -53,20 +64,28 @@ class TestCommentAPI:
         assert response.data["data"]["author"] == setup_data[2].author.nickname
 
     @pytest.mark.django_db
-    def test_create_comment(self, client, logined_client, setup_data):
+    def test_create_comment(
+        self, client, logined_client, setup_data, setup_files, mocked_create_files
+    ):
         endpoint = reverse("community:comment", kwargs={"comment_id": 1})
 
         # 로그인된 유저 테스트
-        response = logined_client[0].post(
-            endpoint,
-            {"content": "testcontent3", "html_content": "htmltestcontent3"},
-            format="json",
-        )
+        with open(setup_files[0].name, "rb") as image:
+            response = logined_client[0].post(
+                endpoint,
+                {
+                    "content": "testcontent3",
+                    "html_content": "htmltestcontent3",
+                    "files": image,
+                },
+                format="multipart",
+            )
 
         assert response.status_code == 201
         assert response.data["data"]["post_id"] == setup_data[0].id
         assert response.data["data"]["author"] == setup_data[4].login_id
         assert Comment.objects.all().count() == 3
+        mocked_create_files.assert_called_once()
 
         # 로그인 안 된 유저 테스트
         response = client.post(
@@ -78,24 +97,49 @@ class TestCommentAPI:
         assert response.status_code == 401
 
     @pytest.mark.django_db
-    def test_update_comment(self, client, logined_client, setup_data):
+    def test_update_comment(
+        self,
+        client,
+        logined_client,
+        setup_data,
+        setup_files,
+        mocked_delete_files,
+        mocked_put_files,
+    ):
         endpoint = reverse("community:comment", kwargs={"comment_id": 2})
 
         # 로그인된 유저 테스트(자신의 댓글)
-        response = logined_client[0].patch(
-            endpoint,
-            {
-                "content": "testcontent_updated",
-                "html_content": "htmltestcontent_updated",
-                "files_state": "DELETE",
-            },
-            format="json",
-        )
+        with open(setup_files[0].name, "rb") as image:
+            response = logined_client[0].patch(
+                endpoint,
+                {
+                    "content": "testcontent_updated",
+                    "html_content": "htmltestcontent_updated",
+                    "files_state": "DELETE",
+                    "files": image,
+                },
+                format="multipart",
+            )
 
         assert response.status_code == 200
         assert response.data["data"]["post_id"] == setup_data[0].id
         assert response.data["data"]["author"] == setup_data[4].login_id
         assert Comment.objects.get(id=2).content == "testcontent_updated"
+        mocked_delete_files.assert_called_once()
+
+        with open(setup_files[0].name, "rb") as image:
+            response = logined_client[0].patch(
+                endpoint,
+                {
+                    "content": "testcontent_updated",
+                    "html_content": "htmltestcontent_updated",
+                    "files_state": "REPLACE",
+                    "files": image,
+                },
+                format="multipart",
+            )
+
+        mocked_put_files.assert_called_once()
 
         # 로그인된 유저 테스트(자신의 댓글) - File state wrong
         response = logined_client[0].patch(
@@ -105,7 +149,7 @@ class TestCommentAPI:
                 "html_content": "htmltestcontent_updated",
                 "files_state": "eeee",
             },
-            format="json",
+            format="multipart",
         )
 
         assert response.status_code == 400
@@ -118,7 +162,7 @@ class TestCommentAPI:
                 "html_content": "htmltestcontent_updated",
                 "files_state": "DELETE",
             },
-            format="json",
+            format="multipart",
         )
 
         assert response.status_code == 403
@@ -131,13 +175,15 @@ class TestCommentAPI:
                 "html_content": "htmltestcontent_updated",
                 "files_state": "DELETE",
             },
-            format="json",
+            format="multipart",
         )
 
         assert response.status_code == 401
 
     @pytest.mark.django_db
-    def test_delete_comment(self, client, logined_client, setup_data):
+    def test_delete_comment(
+        self, client, logined_client, setup_data, mocked_delete_files
+    ):
         endpoint = reverse("community:comment", kwargs={"comment_id": 2})
 
         # 로그인 사용자에 대한 테스트 - 자신의 댓글이 아님
@@ -155,6 +201,7 @@ class TestCommentAPI:
 
         assert response.status_code == 204
         assert response.data["message"] == "Comment deleted successfully"
+        mocked_delete_files.assert_called_once()
 
         with pytest.raises(Comment.DoesNotExist):
             comment = Comment.objects.get(id=2)
