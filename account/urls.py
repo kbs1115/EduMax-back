0da9d662a1f9ab1dca1ffb22615efb5925d025ce
kbs1115 <bruce11158@gmail.com>
2024-02-16 01:45:38 +0900

    main 변경 사항에 맞게 재수정

--- account/urls.py
+++ account/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .view.user_views import *
 from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework import routers
-from account.views import DuplicateCheckerAPIView
+from account.view.user_views import DuplicateCheckerAPIView
 
 app_name = "account"
 
@@ -10,9 +10,13 @@ router = routers.DefaultRouter()
 router.register("test", TestViewSet)
 
 urlpatterns = [
-    path("user/check-duplicate/", DuplicateCheckerAPIView.as_view()),  # 회원가입만을 위한 중복체커여서 확장성 떨어져보여서 맘에안듦
+    path(
+        "user/check-duplicate/", DuplicateCheckerAPIView.as_view()
+    ),  # 회원가입만을 위한 중복체커여서 확장성 떨어져보여서 맘에안듦
     path("user/send-email/", EmailSenderApiView.as_view()),  # 이메일 보내기
-    path("user/email-authenticate/", EmailAuthenticationApiView.as_view()),  # 이메일 인증
+    path(
+        "user/email-authenticate/", EmailAuthenticationApiView.as_view()
+    ),  # 이메일 인증
     path("user/<int:pk>/", CertainUserAPIView.as_view(), name="certainUser"),
     path("user/", UserAPIView.as_view(), name="user"),
     path("token/", AuthAPIView.as_view(), name="token"),
