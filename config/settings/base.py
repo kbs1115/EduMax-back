36c34fc007bf851fdba8d40e8b97dd6a371c3e78
윤규민 <dbsrbals26@gmail.com>
2024-02-27 13:18:00 +0900

    settings 세팅

--- config/settings/base.py
+++ config/settings/base.py
@@ -18,7 +18,7 @@ from decouple import config
 load_dotenv(verbose=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
