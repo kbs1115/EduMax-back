fcd348dc3c192e3be4004d51e99216c55f5458e5
kbs1115 <bruce11158@gmail.com>
2024-02-27 14:55:09 +0900

    DB 연결 설정

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,3 +1,17 @@
 from .base import *
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
 
 ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "edumax",
+        "USER": "root",
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": "localhost",
+        "PORT": "3306",
+    }
+}
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,17 @@
 from .base import *
+from dotenv import load_dotenv
 
-ALLOWED_HOSTS = ["54.79.7.181"]
+load_dotenv(verbose=True)
+
+ALLOWED_HOSTS = ["13.124.25.141"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": "admin",
+        "PASSWORD": os.getenv("RDS_DB_PASSWORD"),
+        "HOST": "database-edumax.cdommqogw444.ap-northeast-2.rds.amazonaws.com",
+        "PORT": "3306",
+    }
+}
