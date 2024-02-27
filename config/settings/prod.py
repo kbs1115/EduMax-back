54acd46abdb6d2173058e62455e445544146c65b
윤규민 <dbsrbals26@gmail.com>
2024-02-27 14:47:13 +0900

    db setting 변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,20 @@
 from .base import *
 
+
 ALLOWED_HOSTS = ['54.79.7.181']
+
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = []
+DEBUG = False
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': '3306',
+    }
+}
