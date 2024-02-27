b27a269a32c0f79a9dc03bf2c5f81b4d81b81b61
kbs1115 <bruce11158@gmail.com>
2024-02-27 14:55:09 +0900

    DB 연결 설정

--- config/settings/base.py
+++ config/settings/base.py
@@ -94,16 +94,6 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "edumax",
-        "USER": "root",
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": "localhost",
-        "PORT": "3306",
-    }
-}
 # 유튜브 api key
 YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
 # celery 세팅
