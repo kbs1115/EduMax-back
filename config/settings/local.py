7eacfe3ae8335a0013b673350a5c833f9a759175
윤규민 <dbsrbals26@gmail.com>
2024-02-27 16:32:44 +0900

    setting 변경

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,3 +1,14 @@
 from .base import *
 
 ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "edumax",
+        "USER": "root",
+        "PASSWORD": os.getenv("LOCAL_DB_PASSWORD"),
+        "HOST": "localhost",
+        "PORT": "3306",
+    }
+}
