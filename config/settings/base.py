43f132802ea6fd0db54313014064d4305d39ec3e
kbs1115 <bruce11158@gmail.com>
2024-03-01 22:30:16 +0900

    CICD pipeline:test push

--- config/settings/base.py
+++ config/settings/base.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
+
+CI/CD applied
 """
 
 from dotenv import load_dotenv
