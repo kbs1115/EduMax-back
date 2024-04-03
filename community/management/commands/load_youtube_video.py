8a137fe8532321417f189a64107ef85a81951e79
윤규민 <dbsrbals26@gmail.com>
2024-04-04 00:56:12 +0900

    [modify] 유튜브id 다운도르 트랜잭션 설정

--- community/management/commands/load_youtube_video.py
+++ community/management/commands/load_youtube_video.py
@@ -1,4 +1,5 @@
 from django.core.management import BaseCommand
+from django.db import transaction
 from googleapiclient.discovery import build
 from django.conf import settings
 from rest_framework.exceptions import ValidationError
@@ -21,40 +22,43 @@ class Command(BaseCommand):
         print("_________________________________________________________________________")
         print("youtube로 부터 video id load를 시작합니다.")
         print("_________________________________________________________________________")
-        for playlist_id, category_list in PLAYLIST_ID_KEY_CATEGORY_VALUE.items():
-            print(f"<<<<playlist_id:{playlist_id}>>>>")
-            while True:
-                playlist_items_request = youtube.playlistItems().list(
-                    part='snippet',
-                    playlistId=playlist_id,
-                    maxResults=50,  # 리소스 요청 최대 개수
-                    pageToken=next_page_token
-                )
-                playlist_items_response = playlist_items_request.execute()
+        try:
+            with transaction.atomic():
+                for playlist_id, category_list in PLAYLIST_ID_KEY_CATEGORY_VALUE.items():
+                    print(f"<<<<playlist_id:{playlist_id}>>>>")
+                    while True:
+                        playlist_items_request = youtube.playlistItems().list(
+                            part='snippet',
+                            playlistId=playlist_id,
+                            maxResults=50,  # 리소스 요청 최대 개수
+                            pageToken=next_page_token
+                        )
+                        playlist_items_response = playlist_items_request.execute()
 
-                category_depth_1 = category_list[0]
-                category_depth_2 = category_list[1]
-                category_depth_3 = category_list[2]
-                category_depth_4 = category_list[3]
-                for item in playlist_items_response['items']:
+                        category_depth_1 = category_list[0]
+                        category_depth_2 = category_list[1]
+                        category_depth_3 = category_list[2]
+                        category_depth_4 = category_list[3]
+                        for item in playlist_items_response['items']:
+                            video_id = item['snippet']['resourceId']['videoId']
+                            title = item['snippet']['title']
+                            admin_user = get_user_with_pk(1)  # 우선 관리자가 올리는걸로..
 
-                    video_id = item['snippet']['resourceId']['videoId']
-                    title = item['snippet']['title']
-                    admin_user = get_user_with_pk(1)  # 우선 관리자가 올리는걸로..
+                            print("title:", title, "playlist_id:", playlist_id)
 
-                    print("title:", title, "playlist_id:", playlist_id)
+                            LectureService.create_lecture(
+                                category_d1=category_depth_1,
+                                category_d2=category_depth_2,
+                                category_d3=category_depth_3,
+                                category_d4=category_depth_4,
+                                title=title,
+                                youtube_id=video_id,
+                                author=admin_user
+                            )
 
-                    LectureService.create_lecture(
-                        category_d1=category_depth_1,
-                        category_d2=category_depth_2,
-                        category_d3=category_depth_3,
-                        category_d4=category_depth_4,
-                        title=title,
-                        youtube_id=video_id,
-                        author=admin_user
-                    )
-
-                # 다음페이지가 있는지 확인
-                next_page_token = playlist_items_response.get('nextPageToken')
-                if not next_page_token:
-                    break
+                        # 다음페이지가 있는지 확인
+                        next_page_token = playlist_items_response.get('nextPageToken')
+                        if not next_page_token:
+                            break
+        except Exception as e:
+            print(f"{e}")
